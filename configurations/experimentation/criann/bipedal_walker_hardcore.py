1e5c4a2372dc6c3335626622da7b8f83ec65bc23
maxalaar <maxalaar@protonmail.com>
2024-12-30 11:34:50 +0100

    MOD : bipedal_walker_hardcore.py

--- configurations/experimentation/criann/bipedal_walker_hardcore.py
+++ configurations/experimentation/criann/bipedal_walker_hardcore.py
@@ -18,22 +18,19 @@ bipedal_walker_hardcore.reinforcement_learning_configuration.number_environment_
 # Reinforcement Learning
 # https://github.com/DLR-RM/rl-baselines3-zoo/blob/master/hyperparams/ppo.yml
 # https://github.com/ovechkin-dm/ppo-lstm-parallel
-bipedal_walker_hardcore.reinforcement_learning_configuration.training_name = 'Dense_V3'
+bipedal_walker_hardcore.reinforcement_learning_configuration.training_name = 'Dense_V4'
 bipedal_walker_hardcore.reinforcement_learning_configuration.architecture = DensePPO
 bipedal_walker_hardcore.reinforcement_learning_configuration.architecture_configuration = {
     'configuration_hidden_layers': [1024, 1024, 1024, 1024],
     'activation_function': LeakyReLU(),
     'layer_normalization': True,
 }
-bipedal_walker_hardcore.reinforcement_learning_configuration.number_gpus_per_learner = 1
-bipedal_walker_hardcore.reinforcement_learning_configuration.number_environment_runners = 6
-bipedal_walker_hardcore.reinforcement_learning_configuration.number_environment_per_environment_runners = 2
 
 bipedal_walker_hardcore.reinforcement_learning_configuration.learning_rate = 3e-4
 bipedal_walker_hardcore.reinforcement_learning_configuration.use_generalized_advantage_estimator = True
 bipedal_walker_hardcore.reinforcement_learning_configuration.lambda_gae = 0.95
-bipedal_walker_hardcore.reinforcement_learning_configuration.train_batch_size = 1024 #40_000
-bipedal_walker_hardcore.reinforcement_learning_configuration.minibatch_size = 1024 #40_000
+bipedal_walker_hardcore.reinforcement_learning_configuration.train_batch_size = 40_000
+bipedal_walker_hardcore.reinforcement_learning_configuration.minibatch_size = 40_000
 bipedal_walker_hardcore.reinforcement_learning_configuration.number_epochs = 128
 bipedal_walker_hardcore.reinforcement_learning_configuration.entropy_coefficient = 0.0
 
